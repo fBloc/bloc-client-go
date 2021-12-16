599d0250b8d905a488a2552d44c96f1e3f019f99
pillipanda <pillipanda@126.com>
2021-12-16 13:39:04 +0800

    🐞 fix(client register api): wrong field of register functions req

--- periodicly_register_functions.go
+++ periodicly_register_functions.go
@@ -33,22 +33,22 @@ type RegisterFuncResp struct {
 }
 
 type RegisterFuncReq struct {
-	Who                         string                        `json:"who"`
-	GroupNameMapFuncNameMapFunc map[string][]*HttpReqFunction `json:"groupName_map_functionName_map_function"`
+	Who                   string                        `json:"who"`
+	GroupNameMapFunctions map[string][]*HttpReqFunction `json:"groupName_map_functions"`
 }
 type GroupNameMapFunctions map[string][]*HttpReqFunction
 
 func (bC *BlocClient) RegisterFunctionsToServer() {
 	req := RegisterFuncReq{
-		Who:                         bC.Name,
-		GroupNameMapFuncNameMapFunc: make(map[string][]*HttpReqFunction)}
+		Who:                   bC.Name,
+		GroupNameMapFunctions: make(map[string][]*HttpReqFunction)}
 
 	for _, funcGroup := range bC.FunctionGroups {
 		groupName := funcGroup.Name
-		req.GroupNameMapFuncNameMapFunc[groupName] = make(
+		req.GroupNameMapFunctions[groupName] = make(
 			[]*HttpReqFunction, len(funcGroup.Functions))
 		for i, function := range funcGroup.Functions {
-			req.GroupNameMapFuncNameMapFunc[groupName][i] = &HttpReqFunction{
+			req.GroupNameMapFunctions[groupName][i] = &HttpReqFunction{
 				Name:          function.Name,
 				GroupName:     function.GroupName,
 				Description:   function.Description,
